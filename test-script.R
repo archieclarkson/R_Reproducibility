2e07c7dc3ebdd55a907546c14073b31c25bbcd2c
archieclarkson <ac1552@exeter.ac.uk>
2025-01-23 12:06:53 +0000

    added .gitignore comments

--- test-script.R
+++ test-script.R
@@ -1,3 +1,12 @@
 # Script 1
 
 #Hey hey
+
+#Go into .gitignore and add files not to add to github
+# Omit certain file types (e.g. powerpoint, pdf, word)
+#*.pptx
+#*.pdf
+#*.docx
+
+# hide raw data example
+#raw-data/*
